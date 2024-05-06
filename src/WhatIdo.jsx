57fa1ab1dd290774c7e5f09f5e36a365c1b90a94
khushbu2020 <139769578+khushbu2020@users.noreply.github.com>
2024-05-06 18:35:16 +0530

    first cahnges

---
+++ src/WhatIdo.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const WhatIdo = () => {
+  return (
+    <div>
+     
+      <section className="text-gray-600 bg-slate-200 mt-3 body-font">
+  <div className="container px-5 py-10 mx-auto">
+    <div className="flex flex-col text-center w-full mb-12">
+      <h1 className="sm:text-3xl cursor-pointer text-2xl font-medium title-font mb-4 text-slate-500">
+       <u>My Skill</u> 
+      </h1>
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+      In addition to my core skills in HTML, CSS, Bootstrap, React.js, Redux, and Tailwind CSS, I excel in version control with Git, enabling efficient collaboration and code management. My expertise lies in crafting responsive and visually appealing interfaces using these technologies. With a strong foundation in JavaScript, I leverage frameworks like React.js and state management with Redux to build dynamic and scalable web applications. I also have a deep understanding of CSS frameworks like Bootstrap and utility-first frameworks like Tailwind CSS, ensuring efficient and maintainable styling solutions.
+      </p>
+    </div>
+   
+  </div>
+</section>
+
+    </div>
+  );
+}
+
+export default WhatIdo;
---
+++ src/component/Step.jsx
@@ -0,0 +1,148 @@
+import React from 'react';
+import WhatIdo from '../WhatIdo';
+
+const Step = () => {
+  return (
+    <div>
+        <WhatIdo/>
+      <section className="text-gray-600  body-font">
+  <div className="container px-5 mx-auto flex flex-wrap">
+    <div className="flex relative pt-4 pb-4 sm:items-center md:w-2/3 mx-auto">
+      <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div className="h-full w-1 bg-gray-200 pointer-events-none" />
+      </div>
+      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-slate-500 text-white relative z-10 title-font font-medium text-sm">
+        1
+      </div>
+      <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+        <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 rounded-full inline-flex items-center justify-center" style={{color:"#f06529"}}>
+          
+          <i className="fa-brands fa-html5 text-6xl"></i>
+        </div>
+        <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+            HTML
+          </h2>
+          <p className="leading-relaxed">
+          Proficient in HTML for structuring web pages.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="flex relative pb-4 sm:items-center md:w-2/3 mx-auto">
+      <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div className="h-full w-1 bg-gray-200 pointer-events-none" />
+      </div>
+      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-slate-500 text-white relative z-10 title-font font-medium text-sm">
+        2
+      </div>
+      <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+        <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+        <i className="fa-brands fa-css3-alt text-6xl" style={{color:"#2965f1"}}></i>
+        </div>
+        <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+            CSS
+          </h2>
+          <p className="leading-relaxed">
+          Skilled in CSS for styling and layout design.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="flex relative pb-4 sm:items-center md:w-2/3 mx-auto">
+      <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div className="h-full w-1 bg-gray-200 pointer-events-none" />
+      </div>
+      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-slate-500 text-white relative z-10 title-font font-medium text-sm">
+        3
+      </div>
+      <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+        <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+        <i className="fa-brands fa-bootstrap text-6xl" style={{color:"#563d7c"}}></i>
+        </div>
+        <div className="flex-shrink-0 w-24 h-24 bg-indigo-100  text-indigo-500  rounded-full inline-flex items-center justify-center">
+        <box-icon type='logo' name='tailwind-css' style={{color:"#3490dc"}}></box-icon>
+        </div>
+        <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+            Bootstrap / Tailwind
+          </h2>
+          <p className="leading-relaxed">
+          Experienced in Bootstrap for rapid development of responsive websites. / Experienced in Tailwind CSS for rapid prototyping and styling.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="flex relative pb-4 sm:items-center md:w-2/3 mx-auto">
+      <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div className="h-full w-1 bg-gray-200 pointer-events-none" />
+      </div>
+      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-slate-500 text-white relative z-10 title-font font-medium text-sm">
+        4
+      </div>
+      <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+       
+        <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+        <i className="fa-brands fa-js text-6xl" style={{color:"#f0db4f"}}></i>
+        </div>
+        <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+            Java Script
+          </h2>
+          <p className="leading-relaxed">
+          Proficient in JavaScript for dynamic and interactive web experiences.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="flex relative pb-4 sm:items-center md:w-2/3 mx-auto">
+      <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div className="h-full w-1 bg-gray-200 pointer-events-none" />
+      </div>
+      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-slate-500 text-white relative z-10 title-font font-medium text-sm">
+        5
+      </div>
+      <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+        <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+        <i className="fa-brands fa-react text-6xl"></i>
+        </div>
+        <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+            React Js
+          </h2>
+          <p className="leading-relaxed">
+          Experienced in React.js for building reusable UI components.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="flex relative pb-4 sm:items-center md:w-2/3 mx-auto">
+      <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div className="h-full w-1 bg-gray-200 pointer-events-none" />
+      </div>
+      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-slate-500 text-white relative z-10 title-font font-medium text-sm">
+        6
+      </div>
+      <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+        <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+        <box-icon type='logo' name='redux'></box-icon>
+        </div>
+        <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+           Redux
+          </h2>
+          <p className="leading-relaxed">
+          Skilled in Redux for managing application state effectively.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+    </div>
+  );
+}
+
+export default Step;
